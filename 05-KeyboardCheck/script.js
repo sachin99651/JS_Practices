a0763739c2bcc033ce9536db4b4e56b27df92093
Sachin Pal <sch99651@gmail.com>
2026-01-31 10:38:50 +0530

    Refactor keydown event listener for clarity

--- 05-KeyboardCheck/script.js
+++ 05-KeyboardCheck/script.js
@@ -5,8 +5,9 @@ window.addEventListener('keydown',(e)=>{
     insert.innerHTML=`<h1>Pressed key is Space</h1>
     <h1>Key Code is ${e.keyCode}</h1>`
   }else{ 
-  insert.innerHTML=`<h1>Pressed key is :${e.key}</h1>
+  insert.innerHTML=`<h1>Pressed key is : ${e.key}</h1>
   <h1>Key Code is ${e.keyCode}</h1>`
   }
 
 })
+
